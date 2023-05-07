a7d4046df41a5cc325b69eeda42912fc148b1576
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-05-07 16:30:35 +0530

    v1 lab Delta 1.14
    Provided the store to the Provider Component in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import store from './store/index'
 import App from './App';
 import reducers from './reducers/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // console.log(reducers)
 root.render(
-  <React.StrictMode>
-    <Provider store={createStore(reducers)}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
