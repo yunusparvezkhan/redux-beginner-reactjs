0db3f31cfda62f478a50d691cdd64922807d7d32
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-05-07 22:59:07 +0530

    v1 Delta 2.3
    some styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SongsList from "./components/SongsList";
 
+import './styles/stylelib.css';
+
 function App() {
   return (
     <div className="ui container grid">
       <div className="ui row">
         <div className="column eight wide" >
-          <SongsList />
+          <SongsList className="songListCom" />
         </div>
       </div>
     </div>
--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectedSongReducer } from '../reducers/reducerSlice';
 
+import '../styles/stylelib.css';
+
 const SongsList = () => {
     const songs = useSelector((state) => state.reducers.songs)
     const selectedSong = useSelector((state) => state.reducers.selectedSong)
@@ -12,8 +14,8 @@ const SongsList = () => {
         return songs.map((song) => {
             return (
                 <div className='item' key={song.id}>
-                    <div className='content'>{song.title}</div>
-                    <div className='right floated content' >
+                    <div className='content songTitle '>{song.title}</div>
+                    <div className='right floated content songList-box-button' >
                         <button className='ui button primary' onClick={() => { dispatch(selectedSongReducer(song)) }}>Select</button>
                     </div>
                     <br />
@@ -22,9 +24,31 @@ const SongsList = () => {
         })
     }
 
+    const renderSelectedSongDetails = () => {
+        if (selectedSong !== null) {
+            return (
+                <div>
+                    <div className='detail-card' >
+                        <h2 className='detail-card-heading' >Selected Song</h2>
+                        <h3 className='detail-card-title' >{selectedSong.title}</h3>
+                        <label>Song Duration: {selectedSong.duration}</label>
+                    </div>
+                </div>
+            )
+        }
+    }
+
     return (
-        <div>
-            {renderSongslist()}
+        <div className='component songListCom' >
+            <div className='songList'>
+                <div className='songList-box'>
+                    <h2>Song List</h2>
+                    {renderSongslist()}
+                </div>
+            </div>
+            <div className='songDetails' >
+                {renderSelectedSongDetails()}
+            </div>
         </div>
     )
 }
